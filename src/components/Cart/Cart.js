9b3a949f3b90550284d0543e6d22e2483cb3c7ca
AparajitaFS <aparajitap@wilcosource.com>
2023-04-12 21:17:43 +0530

    Adding Food Order App using context reducer effect

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,58 @@
+import { useContext } from "react";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemovehandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddhandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {
+        // [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }]
+        cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemovehandler.bind(null, item.id)}
+            onAdd={cartItemAddhandler.bind(null, item)}
+          />
+          // <li>{item.name}</li>
+        ))
+      }
+    </ul>
+  );
+
+  return (
+    <Modal onClose={props.onCloseCart}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onCloseCart}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
